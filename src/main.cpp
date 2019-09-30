34e2a09652662e7bc249c986e8d997bdea7400f9
Stuart Ianna <stuian@protonmail.com>
2019-09-30 18:39:00 +0300

    Fixed error in delayMs, add clock initialisation

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,9 @@ void delayMs(uint16_t ms);
 int main(void){
 
 
+    // Enable the port GPIO peripheral clock
+    SET_BIT(RCC->APB2ENR,RCC_APB2ENR_IOPCEN);
+
     // Set output mode, push-pull, 10MHz max.
     // Multiply pin by four as configuration field is 4 bits wide.
     // Need to subtract 8 from pin as the high control register is used.
@@ -18,18 +21,19 @@ int main(void){
 
         // Set the LED pin and delay
         SET_BIT(LED_PORT->BSRR,0x1<<LED_PIN);
-        delayMs(300);
+        delayMs(500);
 
         // Clear the LED pin and delay
         SET_BIT(LED_PORT->BRR,0x1<<LED_PIN);
-        delayMs(300);
+        delayMs(500);
     }
 }
 
+// Millisecond delay is an estimate
 void delayMs(uint16_t ms){
 
     for(uint16_t i = 0; i < ms; i++){
-        for(uint16_t j = 0; j < 0xFFF; i++){
+        for(uint16_t j = 0; j < 0x1FFF; j++){
             __ASM("NOP");
         }
     }
